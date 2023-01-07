691d0786d45da95c075f3c09e1659f952919b6ba
rounychoubil <crouny15@gmail.com>
2023-01-07 02:24:13 -0500

    option commentaire, vue personalise, css boostrap

--- account/models.py
+++ account/models.py
@@ -2,4 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    # imj=models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    # age=models.IntegerField()
     pass
--- blog/views.py
+++ blog/views.py
@@ -2,24 +2,31 @@ from django.views.generic import ListView, DetailView, FormView
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
+from django.template.response import TemplateResponse
 from .models import Post
+from account.models import CustomUser
 from .forms import PostForm,CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views import View
+from django.shortcuts import get_object_or_404
 
+# home view
 class BlogListView(ListView):
-    model = Post
+    queryset = Post.objects.filter(status="Publique")
     template_name = "blog/home.html"
     
+# personal user view
+class PostListView(LoginRequiredMixin,View):
+    def get_queryset(self, request):
+        return Post.objects.filter(author=request.user)
+    
+    def get(self, request):
+        posts = self.get_queryset(request)
+        context = {'user': request.user, 'post_list': posts}
+        return TemplateResponse(request, 'blog/post_list.html', context)
     
     
-
-class PostListView(ListView):
-    model = Post
-    template_name = "blog/post_list.html"
-
-
-
+#@@@@@@_____Create data form view
 class BlogCreateView(LoginRequiredMixin,CreateView):
     form_class = PostForm
     template_name = "blog/post_new.html"
@@ -28,8 +35,7 @@ class BlogCreateView(LoginRequiredMixin,CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
-
-
+# comment on detail view
 class CommentGetView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
@@ -67,7 +73,7 @@ class BlogDetailView(LoginRequiredMixin,View):
         view = CommentPostView.as_view()
         return view(request,*args, **kwargs)
 
-
+# updat date form view
 class BlogUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Post
     form_class = PostForm
@@ -77,7 +83,7 @@ class BlogUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
         obj = self.get_object()
         return obj.author == self.request.user
     
-
+# dele data
 class BlogDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Post
     #form_class = PostForm
